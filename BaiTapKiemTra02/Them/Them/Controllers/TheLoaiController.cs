160d91460bc00cdc2fbad825b8b98b7d2651408b
quocanhss <https://github.com/quocanhss/asp_wed.git>
2024-09-28 19:30:27 +0700

    v

---
+++ BaiTapKiemTra02/Them/Them/Controllers/TheLoaiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Them.Data;
+using Them.Models;
+
+namespace Them.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TheLoaiController(ApplicationDbContext db)
+        {  
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var theloai = _db.TheLoai.ToList();
+            ViewBag.TheLoai = theloai;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TheLoaiModel theloai)
+        {
+
+            if (ModelState.IsValid)
+            {              
+                //xử lý việc tạo mới
+                _db.TheLoai.Add(theloai);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ BaiTapKiemTra02/Them/Them/Models/TheLoaiModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Them.Models
+{
+    public class TheLoaiModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
